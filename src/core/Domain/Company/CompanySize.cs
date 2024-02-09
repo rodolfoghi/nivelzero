f4b3adb698390bfcd4df8b60528b718ef68758f7
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2024-02-08 23:14:43 -0300

    Fix format

--- src/core/Domain/Company/CompanySize.cs
+++ src/core/Domain/Company/CompanySize.cs
@@ -6,27 +6,27 @@ public enum CompanySize
     /// Headcount of 1-9 
     /// </summary>
     Micro,
-    
+
     /// <summary>
     /// Headcount of 10-49
     /// </summary>
     Small,
-    
+
     /// <summary>
     /// Headcount of 50-99
     /// </summary>
     MediumLow,
-    
+
     /// <summary>
     /// Headcount of 100-249
     /// </summary>
     MediumHigh,
-    
+
     /// <summary>
     /// Headcount of 250-499
     /// </summary>
     LargeLow,
-    
+
     /// <summary>
     /// Headcount of 500 or more
     /// </summary>
--- src/core/Domain/Tenant/PrimaryInterest.cs
+++ src/core/Domain/Tenant/PrimaryInterest.cs
@@ -6,17 +6,17 @@ public enum PrimaryInterest
     /// Use it in my company
     /// </summary>
     UseItInMyCompany,
-    
+
     /// <summary>
     /// Offer Odoo services to other companies
     /// </summary>
     OfferServicesToOtherCompanies,
-    
+
     /// <summary>
     /// I am a student
     /// </summary>
     Student,
-    
+
     /// <summary>
     /// I am a teacher
     /// </summary>
